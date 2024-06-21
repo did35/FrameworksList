21e72a155acfc7f21fdbdfc794dda7bbc0d13525
Didier <did35@yahoo.com>
2024-06-21 09:15:45 +0200

    Initial Commit

---
+++ FrameworksList/FrameworksListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksListApp.swift
+//  FrameworksList
+//
+//  Created by Didier Delhaisse on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FrameworksListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
